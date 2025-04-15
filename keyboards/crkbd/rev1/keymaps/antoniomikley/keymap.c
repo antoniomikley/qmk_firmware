fdc29d11851c14417fd167050872f0774ada240b
= <antonio.mikley@gmail.com>
2025-04-15 10:10:32 +0200

    update keymap

--- keyboards/crkbd/rev1/keymaps/antoniomikley/keymap.c
+++ keyboards/crkbd/rev1/keymaps/antoniomikley/keymap.c
@@ -20,10 +20,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
-      LALT_T(KC_ESC),  KC_Q, KC_W, KC_F, KC_P,  KC_B,        KC_J,   KC_L,  KC_U,         KC_Y,   KC_SCLN, KC_BSPC,
-      LCTL_T(KC_TAB),  KC_A, KC_R, KC_S, KC_T,  KC_G,        KC_M,   KC_N,  KC_E, KC_I,   KC_O,    KC_QUOT,
+      KC_ESC,  KC_Q, KC_W, KC_F, KC_P,  KC_B,        KC_J,   KC_L,  KC_U,         KC_Y,   KC_SCLN, KC_BSPC,
+      LCTL_T(KC_TAB),  KC_A, KC_R, KC_S, KC_T,  KC_G,        KC_M,   KC_N,  KC_E, KC_I,   KC_O,    LALT_T(KC_QUOT),
       KC_LSFT, KC_X, KC_C, KC_D,         KC_V,  KC_Z,        KC_K,   KC_H,  KC_COMM,      KC_DOT, KC_SLSH, KC_RSFT,
-                           KC_LGUI,      LT(1, KC_ESC), KC_ENT,      KC_SPC, MO(2), KC_RGUI
+                           KC_LGUI,      MO(1), KC_ENT,      KC_SPC, MO(2), KC_RGUI
 
   ),
 
@@ -35,16 +35,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
     [2] = LAYOUT_split_3x6_3(
-      XXXXXXX, XXXXXXX, KC_P7, KC_P8, KC_P9, XXXXXXX,             XXXXXXX, KC_PGUP, KC_UP, KC_PGDN, XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, KC_P4, KC_P5, KC_P6, KC_P0,             XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, KC_P1, KC_P2, KC_P3, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, KC_7, KC_8, KC_9, XXXXXXX,             XXXXXXX, KC_PGUP, KC_UP, KC_PGDN, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, KC_4, KC_5, KC_6, KC_0,             XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
+      KC_LSFT, XXXXXXX, KC_1, KC_2, KC_3, XXXXXXX,             XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT,
                                 XXXXXXX, MO(3), XXXXXXX,        XXXXXXX, _______, XXXXXXX
   ),
 
     [3] = LAYOUT_split_3x6_3(
       KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
       XXXXXXX, XXXXXXX, OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LCTL | MOD_LALT), XXXXXXX, KC_INS, KC_DEL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      TG(4), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                           KC_LCTL, _______,  KC_ENT,     KC_SPC, _______, KC_RALT
-  )
+  ),
+
+    [4] = LAYOUT_split_3x6_3(
+        KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T,           KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+        KC_TAB, KC_A, KC_S, KC_D, KC_F, KC_G,           KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        KC_LSFT,KC_Z, KC_X, KC_C, KC_V, KC_B,           KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                    KC_LALT, KC_LCTL, KC_SPACE,     KC_ENT, KC_GRV, TG(4)
+    )
 };
